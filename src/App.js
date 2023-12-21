cd8db4a32efe58c635621e66f1822b7314fe716d
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-12-21 19:44:19 +0300

    completed the sign up page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Signup from "./Pages/Signup";
+import Login from "./Pages/Login";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Router>
+      <div>
+        <section>
+          <Routes>
+            <Route path="/signup" element={Signup} />
+            <Route path="/login" element={Login} />
+          </Routes>
+        </section>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
